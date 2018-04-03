086973973b9971a07fbb207eb1a65f3eb03b9f3c
Vinith <upajapp@gmail.com>
2018-04-03 15:46:41 +0530

    first commit

---
+++ server/models/licence.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose');
+
+var Licence = mongoose.model('Licence', {
+  key: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  keyfor: {
+    type: String,
+    default: "empty"
+  },
+  active: {
+    type: Boolean,
+    default: false
+  },
+  ad: {
+    type: String,
+    default: "empty"
+  },
+  ed: {
+    type: String,
+    default: "empty"
+  },
+  deviceid: {
+    type: String,
+    trim: true,
+    default: "empty"
+  },
+  days: { //actually days
+    type: Number,
+    default: 1
+  },
+  _creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  }
+});
+
+module.exports = {Licence};
